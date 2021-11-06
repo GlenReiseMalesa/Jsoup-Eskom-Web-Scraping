40398a128dacf9ad1a854f3245e88771e32475cc
GlenReiseMalesa <malesaglen6@gmail.com>
2021-11-06 14:26:22 +0200

    function for unorganized loadshedding schedule for only one subrub

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Main {
 
 	
@@ -10,11 +18,58 @@ public class Main {
 		//accessing the location data
            Location l  = new Location();     
            System.out.println(Location.location.get(10000).province);
-			
+           
+           
+           
+        //Getting the loadshedding schedule 
+       	 System.out.println("Loadshedding Stage "+loadSheddingStatus());
+       	 
+       	 //Getting the schedule of each surburb
+       	LoadSheddingSchedule("https://loadshedding.eskom.co.za/LoadShedding/GetScheduleM/"+Location.location.get(10000).surburbIndex+"/"+loadSheddingStatus()+"/1/704",Location.location.get(10000).surburb);
+       	 
+       	  	 
+       	 
+       	 
 		
 	}
 	
 	
+	
+	public static void LoadSheddingSchedule(String url,String surburb) {
+		
+        Document doc;
+        
+		try {
+			doc = Jsoup.connect(url).get();
+	           Elements statusText = doc.getElementsByClass("scheduleDay").select("div");
+	           String schedule = statusText.text().toString(); 
+	           
+	           System.out.println(schedule);
+	         
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		
+		}
+		
+	}
+	
+	public static int loadSheddingStatus() {
+        Document doc;
+		try {
+			doc = Jsoup.connect("https://www.eskom.co.za/").get();
+	           Elements statusText = doc.getElementsByClass("eskom-ls-text").select("b");
+	           String status = statusText.text().toString(); 
+	           
+	           //get the stage number
+	           char stat = status.charAt(status.length() - 1);
+	           
+	           return Integer.parseInt(stat+"");
+		} catch (IOException e) {
+			e.printStackTrace();
+			   return 1;	
+		}
+	}
 	
 	
 
